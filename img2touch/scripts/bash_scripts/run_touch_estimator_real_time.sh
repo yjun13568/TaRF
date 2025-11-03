585e9e379e00db1ff81aa24a402af41f09d1eb90
junwon <yjun13568@snu.ac.kr>
2025-11-03 18:09:15 +0900

    initial

--- img2touch/scripts/bash_scripts/run_touch_estimator_real_time.sh
+++ img2touch/scripts/bash_scripts/run_touch_estimator_real_time.sh
@@ -1,12 +1,13 @@
-export CUDA_VISIBLE_DEVICES=6
+export CUDA_VISIBLE_DEVICES=0
+export PYTORCH_CUDA_ALLOC_CONF="max_split_size_mb:32"
 python scripts/touch_estimator_real_time.py \
     --indir ../nerfstudio_modules/outputs/touch_estimation_input_cache \
-    --bg_path touch_bg/bench_outdoor_1_colmap_40_50/bg.jpg\
+    --bg_path touch_bg/bench_colmap_40_50/bg.jpg\
     --outdir outputs/touch_estimator_real_time/ \
     --ddim_steps 200 \
     --config configs/tarf.yaml \
     --diffusion_ckpt pretrained_models/img2touch.ckpt \
     --ranking_rgb_enc_ckpt pretrained_models/reranking_rgb_enc.ckpt \
     --ranking_tac_enc_ckpt pretrained_models/reranking_tac_enc.ckpt \
-    --n_samples 16 \
+    --n_samples 8 \
     --scale 7.5
